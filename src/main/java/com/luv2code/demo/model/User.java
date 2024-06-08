2865019bb3500bc8b2d2e5ca41a52d272d3bd887
hels237 <m7hels@gmail.com>
2024-06-08 15:04:35 +0100

    add user and address entity

---
+++ src/main/java/com/luv2code/demo/model/User.java
@@ -0,0 +1,37 @@
+package com.luv2code.demo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "_user")
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "firstname")
+    private String firstname;
+
+    @Column(name = "lastname")
+    private String lastname;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @OneToOne(mappedBy = "user")
+    private Address address;
+
+
+}
